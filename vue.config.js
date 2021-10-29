2c31ba5d31f7f80da08445e87fb38b8cccb1f743
Enrique Marin <emarin@applab.mx>
2021-10-29 10:33:41 -0600

    add Vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+    //disableHostCheck: true, 
+    host: '0.0.0.0',
+    port: 8080,
+    public: 'tours4friends.com.mx'
+  },
 }
